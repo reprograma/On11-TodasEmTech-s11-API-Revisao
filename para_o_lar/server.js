585a2c059f18168a501a74149c3f5c8a61000159
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-21 15:36:12 -0300

    Refatorando o server.js

--- para_o_lar/server.js
+++ para_o_lar/server.js
@@ -1,7 +1,6 @@
-const app = require('./src/app')
-
+const app = require("./src/app")
 const PORT = 8080 
 
 app.listen(PORT, ()=>{
-    console.log(`rodando na porta ${PORT}`);
+    console.log(`App rodando no endereço http://localhost:${PORT}`)
 })
