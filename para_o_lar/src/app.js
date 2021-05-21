9c9e2d136e953ae949e9121cc752e1b7a4832c70
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-21 15:42:17 -0300

    Refatorado o arquivo app.js

--- para_o_lar/src/app.js
+++ para_o_lar/src/app.js
@@ -1,16 +1,11 @@
-const express = require('express')
+const express = require("express") //chama o express
+const cors = require("cors") //chama o cors
+const estabelecimentos = require("./routes/estabelecimentosRoutes") //chamando as rotas
 
-const cors = require('cors')
+const app = express() //executa o express
 
-const estabelecimentos = require('./routes/estabelecimentosRoutes')
-
-
-const app = express()
-
-app.use(express.json()) // o que vem da requisição é texto livre e estou convertendo no json
-
+app.use(express.json()) //o que vem da requisição é texto livre, aqui converte para json
 app.use(cors())
-
-app.use('/estabelecimentos', estabelecimentos)
+app.use('/estabelecimentos', estabelecimentos) //definindo a rota raiz
 
 module.exports = app
