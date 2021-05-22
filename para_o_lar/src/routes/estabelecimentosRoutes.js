c21c724c04b562ce1c57f196280986cfb426ccf1
Eliane <elianevia@gmail.com>
2021-05-22 20:16:49 -0300

    Para o lar semana 11 - Complemento

--- para_o_lar/src/routes/estabelecimentosRoutes.js
+++ para_o_lar/src/routes/estabelecimentosRoutes.js
@@ -1,18 +1,16 @@
-const controller = require("../controllers/estabelecimentoControllers")
+const controller = require('../controllers/estabelecimentosControllers')
 
-const express = require("express")
+const express = require('express')
 
 const router = express.Router()
 
-router.post("/criar", controller.create)
+router.post('/criar', controller.create)
+router.get('/todos', controller.getAll)
+router.get('/:id', controller.getById)
+router.delete('/:id', controller.remove)
+router.put('/:id', controller.replace)
+router.patch('/:id', controller.update)
 
-router.get("/todos", controller.getAll)
-router.get("/:id", controller.get)
-
-router.delete("/:id", controller.remove)
-
-router.put("/:id", controller.replace)
-
-router.patch("/:id", controller.update)
+router.post('/:id/like', controller.like)
 
 module.exports = router
