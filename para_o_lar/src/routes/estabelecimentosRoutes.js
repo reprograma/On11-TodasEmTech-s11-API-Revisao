d4c9d3df147d985007365af67e7abc894db62d19
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-22 23:19:39 -0300

    Modificação na rota

--- para_o_lar/src/routes/estabelecimentosRoutes.js
+++ para_o_lar/src/routes/estabelecimentosRoutes.js
@@ -1,14 +1,16 @@
-const controller = require('../controllers/estabelecimentosControllers')
+const express = require('express') //chama o express
+const controller = require('../controllers/estabelecimentosControllers') //chama o controller
+const router = express.Router() //executa o Router
 
-const express = require('express')
+//configurando a continuação das rotas e informando qual função irá usar do controller
+router.get("/todos", controller.getAll)
+router.get("/:id", controller.getById)
 
-const router = express.Router()
+router.post("/criar", controller.create)
+router.put("/substituir/:id", controller.replace)
+router.patch("/atualizar/:id", controller.update)
+router.delete("/remover/:id", controller.remove)
 
-router.post('/criar', controller.create)
-router.get('/todos', controller.getAll)
-router.get('/:id', controller.get)
-router.delete('/:id', controller.remove)
-router.put('/:id', controller.replace)
-router.patch('/:id', controller.update)
+router.post('/:id/like', controller.like)
 
 module.exports = router
