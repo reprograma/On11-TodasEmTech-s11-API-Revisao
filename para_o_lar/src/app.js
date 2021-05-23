eb239447a9fb0f349ec29e2deb06c0576a50bdc2
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2021-05-22 23:08:25 -0300

    Modificações no arquivo app.js

--- para_o_lar/src/app.js
+++ para_o_lar/src/app.js
@@ -6,6 +6,6 @@ const app = express() //executa o express
 
 app.use(express.json()) //o que vem da requisição é texto livre, aqui converte para json
 app.use(cors())
-app.use('/estabelecimentos', estabelecimentos) //definindo a rota raiz
+app.use("/estabelecimentos", estabelecimentos) //definindo a rota raiz
 
 module.exports = app
